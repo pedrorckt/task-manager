67740dffd5bff651e18a16eab6ff990c5c30c66f
Pedro Emilio Guglielmo <pedro@rckt.com.br>
2023-03-18 19:46:19 -0300

    Task interface

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common/decorators';
+import { Task } from './task.module';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
 }
